661f5da594fd0dd6cd6a82717925432dd7547390
Muhammad Andre Syahli <mandresyahliii16@gmail.com>
2024-11-19 16:01:28 +0700

    $config['base_url']

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/codeigniter-restserver-3/';
+$config['base_url'] = 'http://localhost/simple-rest-api-codeigniter-3/';
 
 /*
 |--------------------------------------------------------------------------
