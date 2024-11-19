b1ba182cc5473146d35990b4a22102089dcaa8c7
Muhammad Andre Syahli <mandresyahliii16@gmail.com>
2024-11-19 16:40:46 +0700

    get_data

--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -7,7 +7,7 @@ class Mahasiswa_model extends CI_Model
         if ($id === null) {
             return $this->db->get('mahasiswa')->result_array();
         } else {
-            return $this->db->get_where('mahasiswa', ['id' => $id])->row_array();
+            return $this->db->get_where('mahasiswa', ['id' => $id])->result_array();
         }
     }
 
